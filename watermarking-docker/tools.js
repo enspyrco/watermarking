ba5154d64ea6a202d3f74baccaac4871e8102105
Nick Meinhold <nick.meinhold@gmail.com>
2017-01-18 17:07:00 +1100

    Added a tools.js file for keeping a set of utility functions.

---
+++ watermarking-docker/tools.js
@@ -0,0 +1,13 @@
+http://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
+
+// tools.js
+// ========
+module.exports = {
+  
+  foo: function () {
+    // whatever
+  },
+  bar: function () {
+    // whatever
+  }
+};
