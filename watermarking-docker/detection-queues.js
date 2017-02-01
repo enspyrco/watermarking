5b56e3e510d50428c2e9e8a8ad5886405a6a4670
Nick Meinhold <Nick.Meinhold@gmail.com>
2017-02-01 20:44:44 +1100

    Completed detection queues and removed old listener.js detection code.

--- watermarking-docker/detection-queues.js
+++ watermarking-docker/detection-queues.js
@@ -13,6 +13,8 @@ var queueRef = firebaseAdmin.database().ref('queue');
 
 var tools = require('./tools');
 
+var detectingRef = firebaseAdmin.database().ref("detecting/incomplete");
+
 module.exports = {
 
 	setup: function () {
@@ -34,6 +36,9 @@ module.exports = {
 
 			console.log('Downloading original image file for detection, from gcs at location '+data.pathOriginal+', to '+originalPath); 
 
+			// Update progress for webapp UI 
+      		detectingRef.child(data.uid).child('progress').set('Server has received request, downloading original image from storage...');
+
 			// download with gsutil 
     		execFile('gsutil', ['cp', 'gs://watermarking-print-and-scan.appspot.com/'+data.pathOriginal, originalPath], function(error, stdout, stderr){
       
@@ -42,27 +47,14 @@ module.exports = {
       			console.log('Downloaded original image.');
   				
   				// Update progress for webapp UI 
-      			detectingRef.child(snapshot.key).child('progress').set('Server downloaded original image, now downloading marked image...');
+      			detectingRef.child(data.uid).child('progress').set('Server downloaded original image, now downloading marked image from storage...');
 
-		  // console.log('Downloading image named: '+data.name+' for marking, from gcs at location: '+data.path);
+      			// add data to task that is required for next step and resolve 
+      			data.originalPath = originalPath;
+		  		data._new_state = 'download_original_spec_finished';
+		  		resolve(data);
 
-		  // // create a timestamp so we can store the marked image with a unique path 
-		  // var timestamp = String(Date.now());
-		  // var filePath = '/tmp/'+timestamp+'/'+data.name;
-
-		  // console.log('Saving file for marking onto the server at location: '+filePath); 
-
-		  // // execFile: executes a file with the specified arguments
-		  // execFile('gsutil', ['cp', 'gs://watermarking-print-and-scan.appspot.com/'+data.path, filePath], function(error, stdout, stderr){
-
-		  //   if (error) reject(error); 
-
-		  //   data.filePath = filePath; 
-		  //   data.timestamp = timestamp;
-		  //   data._new_state = 'download_for_marking_spec_finished';
-		  //   resolve(data);
-
-		  });
+		  	});
 
 		});
 
@@ -77,66 +69,59 @@ module.exports = {
 		};
 		var downloadMarkedQueue = new Queue(queueRef, downloadMarkedQueueOptions, function(data, progress, resolve, reject) {
 		  
-			var markedPath = '/tmp/'+snapshot.key+'/marked';
+			var markedPath = '/tmp/'+data.uid+'/marked';
 
 			console.log('Saving marked image file for detection, from gcs at location '+data.pathMarked+', to '+markedPath);
 
-		  // console.log('Marking image at location: '+data.filePath+' with message '+data.message+' at strength '+data.strength);
+			execFile('gsutil', ['cp', 'gs://watermarking-print-and-scan.appspot.com/'+data.pathMarked, markedPath], function(error, stdout, stderr){
+      
+        		if (error) reject(error); 
 
-		  // execFile('./mark-image', [data.filePath, data.name, data.message, data.strength], function(error, stdout, stderr){
-		      
-		  //   if (error) reject(error); 
+        		console.log('Downloaded marked image.');
+        
+        		// Update progress for webapp UI 
+        		detectingRef.child(data.uid).child('progress').set('Server has downloaded both images, now detecting watermarks...');
 
-		  //   // Pass out stdout for docker log 
-		  //   console.log('Marked image.\n'+stdout);
+        		// add data to task that is required for next step and resolve 
+      			data.markedPath = markedPath;
+		  		data._new_state = 'download_marked_spec_finished';
+		  		resolve(data);
 
-		  //   data._new_state = 'mark_image_spec_finished';
-		  //   resolve(data); 
-
-		  // });
+		  	});
 
 		});
 
 		/////////////////////////////////////////////////////
 		//
-		// third marking queue - uploads marked image 
+		// third detection queue - performs detection 
 		// 
 		/////////////////////////////////////////////////////
 
-		var uploadMarkedImageQueueOptions = {
-		  'specId': 'upload_marked_image_spec'
+		var performDetectionQueueOptions = {
+		  'specId': 'perform_detection_spec'
 		};
-		var uploadMarkedImageQueue = new Queue(queueRef, uploadMarkedImageQueueOptions, function(data, progress, resolve, reject) {
+		var performDetectionQueue = new Queue(queueRef, performDetectionQueueOptions, function(data, progress, resolve, reject) {
 		  
-		  console.log("Uploading marked image...");
+		  	console.log('Detecting message...');
 
-		  var markedFileGCSPath = 'marked-images/'+data.uid+'/'+data.timestamp+'/'+data.name+'.png';
+		  	execFile('./detect-wm', [data.uid, data.originalPath, data.markedPath], function(error, stdout, stderr){
+    
+	          	if (error) reject(error); 
 
-		  execFile('gsutil', ['cp', data.filePath+'-marked.png', 'gs://watermarking-print-and-scan.appspot.com/'+markedFileGCSPath], function(error, stdout, stderr){
-		      
-		    if (error) reject(error); 
+	          	// Read in the output.json file 
+	          	var resultsJson = JSON.parse(fs.readFileSync('/tmp/'+data.uid+'.json', 'utf8'));
 
-		    // Pass out stdout for docker log 
-		    console.log('Uploaded marked image.\n'+stdout);
+	          	console.log('Detected watermark.');
+	          
+	          	// Update progress for webapp UI 
+	          	detectingRef.child(data.uid).child('progress').set('Detection complete.');
+	          	detectingRef.child(data.uid).child('results').set(resultsJson);
+	          
+	          	console.log('Message detected and results saved to database.');
 
-		    var updateBDCallback = function(urlString) {
-		        // Create a new 'marked' entry 
-		        var markedImageRef = firebaseAdmin.database().ref('/original-images/'+data.uid+'/'+data.imageSetKey+'/marked-images/'+data.markedImageKey);
-		        markedImageRef.update({
-		          message: data.message,
-		          name: data.name,
-		          path: markedFileGCSPath,
-		          strength: data.strength, 
-		          servingUrl: urlString
-		        });
-		        
-		        resolve();
+	          	resolve();
 
-		    };
-
-		    tools.getServingUrl(markedFileGCSPath, updateBDCallback);
-
-		  });
+	       	});
 
 		});
 
--- watermarking-docker/listener.js
+++ watermarking-docker/listener.js
@@ -4,106 +4,28 @@ var firebaseAdminSingleton = require('./firebase-admin-singleton');
 var firebaseAdmin = firebaseAdminSingleton.getAdmin();
 
 var Queue = require('firebase-queue');
-// var admin = require('firebase-admin');
-
-var tools = require('./tools');
-var markingQueues = require('./marking-queues');
-
-markingQueues.setup(); 
 
 // setup variables for running shell script and reading files 
 var execFile = require('child_process').execFile;
 var fs = require('fs');
 
-var detectingRef = firebaseAdmin.database().ref("detecting/incomplete");
-var incompleteDetectingLogsRef = firebaseAdmin.database().ref("logs/detecting/incomplete");
-// Retrieve new 'detect' entries as they are added to our database 
-detectingRef.on("child_added", function(snapshot, prevChildKey) {
-  var detectingEntry = snapshot.val();
-  if(detectingEntry.originalPath !== null) {
+var tools = require('./tools');
 
-    // check if there has already been an attempt 
-    if(detectingEntry.attempts > 0) {
-      // log the problematic entry 
-      console.log("A previous attempt was already made to detect a message in file "+detectingEntry.pathOriginal);
-      console.log("Incomplete detection entry is being removed and logged.");
-      incompleteDetectingLogsRef.push(detectingEntry); 
-      // remove the problematic entry 
-      detectingRef.set(null);
-      return;
-    }
-    
-    // increment the attempts 
-    detectingRef.child(snapshot.key).child('attempts').set(detectingEntry.attempts+1); 
-    
-    detectingRef.child(snapshot.key).child('progress').set('Server has received request and is downloading images...');
+var markingQueues = require('./marking-queues');
+var detectionQueues = require('./detection-queues');
 
-    var originalPath = '/tmp/'+snapshot.key+'/original';
-    var markedPath = '/tmp/'+snapshot.key+'/marked';
+console.log('Setting up queues...');
 
-    console.log("Saving original image file for detection, from gcs at location \'"+detectingEntry.pathOriginal+"\', to \'"+originalPath+"\'."); 
-
-    // execFile: executes a file with the specified arguments
-    execFile('gsutil', ['cp', 'gs://watermarking-print-and-scan.appspot.com/'+detectingEntry.pathOriginal, originalPath], function(error, stdout, stderr){
-      
-      if (error) { // update the db entry with the error 
-        detectingRef.child(snapshot.key).child('error').set('Error downloading original image: '+error);
-        console.log('Error downloading original image: '+error);
-        return;
-      }
-      
-      // Pass out stdout for docker log 
-      console.log('Downloaded original image.\n'+stdout);
-      // Update progress for webapp UI 
-      detectingRef.child(snapshot.key).child('progress').set('Server downloaded original image, now downloading marked image...');
-
-      console.log("Saving marked image file for detection, from gcs at location \'"+detectingEntry.pathMarked+"\', to \'"+markedPath+"\'."); 
-
-      execFile('gsutil', ['cp', 'gs://watermarking-print-and-scan.appspot.com/'+detectingEntry.pathMarked, markedPath], function(error, stdout, stderr){
-      
-        if (error) { // update the db entry with the error 
-          detectingRef.child(snapshot.key).child('error').set('Error downloading marked image: '+error);
-          console.log('Error downloading marked image: '+error);
-          return;
-        }
-
-        // Pass out stdout for docker log 
-        console.log('Downloaded marked image.\n'+stdout);
-        // Update progress for webapp UI 
-        detectingRef.child(snapshot.key).child('progress').set('Server downloaded images, detecting watermark...');
-
-        console.log('Detecting message...');
-
-        execFile('./detect-wm', [snapshot.key, originalPath, markedPath], function(error, stdout, stderr){
-    
-          if (error) { // update the db entry with the error 
-            detectingRef.child(snapshot.key).child('error').set('Error detecting watermark: '+error);
-            console.log('Error detecting watermark: '+error);
-            return;
-          }
-
-          // Read in the output.json file 
-          var resultsJson = JSON.parse(fs.readFileSync('/tmp/'+snapshot.key+'.json', 'utf8'));
-
-          // Pass out stdout for docker log 
-          console.log('Detected watermark.\n'+stdout);
-          // Update progress for webapp UI 
-          detectingRef.child(snapshot.key).child('results').set(resultsJson);
-          
-          console.log('Message detected and results saved to database.');
-
-        });
-
-      });
-
-    });
-
-  }
-});
-
-console.log('Listeners initialised. Setting up queue...');
+markingQueues.setup(); 
+detectionQueues.setup();
 
 var queueRef = firebaseAdmin.database().ref('queue');
+
+/////////////////////////////////////////////////////
+//
+// get serving url queue - for original image 
+// 
+/////////////////////////////////////////////////////
 var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
 
   console.log("Task added to queue: "+data);
@@ -125,6 +47,11 @@ var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
 
 });
 
+/////////////////////////////////////////////////////
+//
+// verify users queue - when admin verifies a user, add their info to db 
+// 
+/////////////////////////////////////////////////////
 var verifyUsersQueueOptions = {
   'specId': 'spec_1'
 };
@@ -150,8 +77,6 @@ var verifyUsersQueue = new Queue(queueRef, verifyUsersQueueOptions, function(dat
   resolve();
 
 });
-
-
 
 console.log('Queue setup finished.');
 
