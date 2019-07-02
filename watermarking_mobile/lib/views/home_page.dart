b183e97e535e72dce519f8cf60352aa1d6fb1c7f
Nick Meinhold <nick.meinhold@gmail.com>
2019-07-02 15:43:08 +1000

    Download images separately for height and width
    - used Image.network's ImageProvider.resolve function to pass callback
    - added callback that uses image data to get height and width

--- watermarking_mobile/lib/views/home_page.dart
+++ watermarking_mobile/lib/views/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
@@ -23,6 +26,11 @@ class _HomePageState extends State<HomePage> {
               scrollDirection: Axis.horizontal,
               itemCount: viewModel.images.length,
               itemBuilder: (BuildContext context, int index) {
+                Image image = Image.network(viewModel.images[index].url);
+                Completer<ui.Image> completer = Completer<ui.Image>();
+                image.image.resolve(ImageConfiguration()).addListener(
+                    (ImageInfo info, bool _) => completer.complete(info.image));
+
                 // use box decoration to indicate selected status
                 BoxDecoration boxDecoration =
                     (viewModel.images[index] == viewModel.selectedImage)
@@ -31,21 +39,35 @@ class _HomePageState extends State<HomePage> {
 
                 return Row(
                   children: <Widget>[
-                    InkWell(
-                      child: Container(
-                        height: 100,
-                        width: 100,
-                        decoration: boxDecoration,
-                        child: Image.network(
-                          viewModel.images[index].url,
-                          fit: BoxFit.scaleDown,
+                    Column(
+                      children: <Widget>[
+                        InkWell(
+                          child: Container(
+                            height: 100,
+                            width: 100,
+                            decoration: boxDecoration,
+                            child: image,
+                          ),
+                          onTap: () {
+                            StoreProvider.of<AppState>(context).dispatch(
+                                ActionSetSelectedImage(
+                                    image: viewModel.images[index]));
+                          },
                         ),
-                      ),
-                      onTap: () {
-                        StoreProvider.of<AppState>(context).dispatch(
-                            ActionSetSelectedImage(
-                                image: viewModel.images[index]));
-                      },
+                        FutureBuilder<ui.Image>(
+                          future: completer.future,
+                          builder: (BuildContext context,
+                              AsyncSnapshot<ui.Image> snapshot) {
+                            if (snapshot.hasData) {
+                              return new Text(
+                                '${snapshot.data.width}x${snapshot.data.height}',
+                              );
+                            } else {
+                              return new Text('Loading...');
+                            }
+                          },
+                        ),
+                      ],
                     ),
                   ],
                 );
